8ed4b4b98d02f8f9049e41715979e474fa354c0b
Kevin Zheng <jxzheng@uwaterloo.ca>
2022-08-21 00:05:12 -0400

    make_purchase functionality

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 # Backend API
 
 from flask import Flask, Response, request
+from flask_cors import CORS
 from twilio.rest import Client
 import pymongo
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Database Vars
 
@@ -24,8 +26,8 @@ twilio_client = Client(twilio_account_sid, twilio_auth_token)
 twilio_phone_number = '+12058399786'
 
 # Phone numbers who we are expecting responses from
-
-phone_numbers_being_waited_for = []
+# key = phone number, value = price
+pending_responses = {}
 
 @app.route("/")
 def index():
@@ -41,13 +43,13 @@ def set_daily_spend_limit():
     patient_record = get_patient_record(patient)
     # Basic authentication: check if the guardian is actually associated with that patient
     if(not patient_record["guardian"] == guardian):
-        return Response("{'error', 'not authorized'}", status=403, mimetype='application/json')
+        return Response("{'error': 'not authorized'}", status=403, mimetype='application/json')
 
     new_spend_limit = { "$set": {"daily_spend_limit": limit} }
     patient_query = {"username": patient}
     db_users.update_one(patient_query, new_spend_limit)
 
-    return Response("{'success', 'authorized'}", status=200, mimetype='application/json')
+    return Response("{\"success\": \"authorize\"}", status=200, mimetype='application/json')
 
 # Usage: /get_daily_limit?patient=<patient_username>
 @app.route("/get_daily_limit")
@@ -55,7 +57,7 @@ def get_daily_spend_limit():
     patient = request.args.get('patient')
     spend_limit = get_patient_record(patient)["daily_spend_limit"]
 
-    return Response(f"{{'daily_limit', '{spend_limit}'}}", status=200, mimetype='application/json')
+    return Response(f"{{\"daily_limit\": {spend_limit}}}", status=200, mimetype='application/json')
 
 # Usage: /get_remaining_daily_limit?patient=<patient_username>
 @app.route("/get_remaining_daily_limit")
@@ -63,7 +65,7 @@ def get_remaining_daily_spend_limit():
     patient = request.args.get('patient')
     remaining_spend_limit = get_patient_record(patient)["remaining_spend_limit"]
 
-    return Response(f"{{'remaining_spend_limit', '{remaining_spend_limit}'}}", status=200, mimetype='application/json')
+    return Response(f"{{\"remaining_spend_limit\": {remaining_spend_limit}}}", status=200, mimetype='application/json')
 
 # Usage: /get_managed_balance?patient=<patient_username>
 @app.route("/get_managed_balance")
@@ -71,7 +73,7 @@ def get_managed_balance():
     patient = request.args.get('patient')
     managed_balance = get_patient_record(patient)["managed_account_balance"]
 
-    return Response(f"{{'managed_balance', '{managed_balance}'}}", status=200, mimetype='application/json')
+    return Response(f"{{\"managed_balance\": {managed_balance}}}", status=200, mimetype='application/json')
 
 # Usage: /make_purchase?patient=<patient_username>&price=<int>
 @app.route("/make_purchase")
@@ -81,18 +83,35 @@ def make_purchase():
     patient_record = get_patient_record(patient)
     guardian_phone_number = patient_record["guardian_phone_number"]
     patient_name = patient_record["full_name"]
+    remaining_spend_limit = float(patient_record["remaining_spend_limit"])
+    managed_account_balance = float(patient_record["managed_account_balance"])
 
-    if(float(patient_record["managed_account_balance"]) < price):
-        return Response("{'error', 'not enough funds'}", status=403, mimetype='application/json')
+    #if(float(patient_record["managed_account_balance"]) < price):
+    #    return Response("{'error', 'not enough funds'}", status=403, mimetype='application/json')
 
-    if(float(patient_record["daily_spend_limit"]) < price):
+    if(remaining_spend_limit < price):
         message = twilio_client.messages.create(
             body=f'HeimWallet: Authorize ${price} transaction from {patient_name}? Location: (insert location)\n\nReply YES to authorize, NO to decline',
             from_=twilio_phone_number,
             to=guardian_phone_number
         )
+        pending_responses[guardian_phone_number] = price
+        return Response(f"{{\"status\": \"req_auth\", \"remaining_spend_limit\": null, \"managed_balance\": null}}", status=200, mimetype='application/json')
 
-    return Response(f"{{'request_auth', '{guardian_phone_number}'}}", status=200, mimetype='application/json')
+    # If we get here, the patient had enough to spend
+
+    remaining_spend_limit = remaining_spend_limit - price
+    managed_account_balance = managed_account_balance - price
+
+    new_limits_and_balance = { "$set": {"remaining_spend_limit": remaining_spend_limit, "managed_account_balance": managed_account_balance} }
+    patient_query = {"username": patient}
+    db_users.update_one(patient_query, new_limits_and_balance)
+    return Response(f"{{\"status\": \"success\", \"remaining_spend_limit\": {remaining_spend_limit}, \"managed_balance\": {managed_account_balance}}}", status=200, mimetype='application/json')
+
+# Twilio calls this function in order to report a YES or NO
+#@app.route("/sms_authorize_response")
+#def sms_authorize_response():
+
 
 # Internal helper function to get patient record from database
 def get_patient_record(patient):
